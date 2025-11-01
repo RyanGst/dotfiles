1abe0ed835e125b5ac0e9287c65d409934935534
Ryan <ryangst.hire@gmail.com>
2025-11-01 18:11:18 -0300

    Add .config/nvim/.repro/data/nvim/lazy/blink.cmp/lua/blink/.keep Update .config/nvim/lazy-lock.json Update .config/nvim/lazyvim.json Update .config/nvim/lua/config/keymaps.lua

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>fN", function()
+  local dir = vim.fn.expand("%:h")
+  vim.ui.input({ prompt = "New file name: ", default = dir .. "/" }, function(input)
+    if input and #input > 0 then
+      vim.cmd("edit " .. vim.fn.fnameescape(input))
+    end
+  end)
+end, { desc = "Create new file in same directory" })

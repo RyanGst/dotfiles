e802ce2730101a14eca37857bef055858027b2a6
Ryan Machado <me@ryanmac.dev>
2026-07-02 16:40:57 -0300

    Update .config/bat/config Add .config/bat/themes/Catppuccin Latte.tmTheme Add .config/delta/catppuccin.gitconfig Update .config/nvim/lazy-lock.json Update .config/nvim/lua/config/lazy.lua Update .config/nvim/lua/config/options.lua Add .config/nvim/lua/plugins/colorscheme.lua Update .gitconfig Update .tmux.conf Add Library/Application Support/com.mitchellh.ghostty/config

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -19,7 +19,7 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = {
-      colorscheme = "tokyonight-night",
+      colorscheme = "catppuccin-latte",
     } },
     -- import/override with your plugins
     { import = "plugins" },
@@ -33,7 +33,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
+  install = { colorscheme = { "catppuccin-latte", "habamax" } },
   checker = {
     enabled = true, -- check for plugin updates periodically
     notify = false, -- notify on update
---
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "latte",
+      background = {
+        light = "latte",
+        dark = "latte",
+      },
+    },
+  },
+}
